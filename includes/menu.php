bb80e27df25d1951fd59712259cca8f905ce4133
thejope <joaomonte19761@gmail.com>
2025-07-20 14:36:53 -0300

    style: estilizando com bootstrap para não ficar tão feio

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,21 @@
-<nav>
-    <a href="painel.php">Painel</a> |
-    <a href="cadastro.php">Cadastro</a> |
-    <a href="usuarios.php">Listar Usuários</a> |
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="painel.php">Painel</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cadastro.php">Cadastro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="usuarios.php">Listar Usuários</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
--- views/cadastro.php
+++ views/cadastro.php
@@ -1,30 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Cadastro de Usuário</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
+
 <body>
-    
-<?php require_once '../includes/menu.php'; ?>
 
+    <?php require_once '../includes/menu.php'; ?>
 
-<h2>Cadastro de Usuário</h2>
-<form id="form-cadastro">
-    <input type="text" name="nome" id="nome" placeholder="Nome"><br><br>
-    <input type="email" name="email" id="email" placeholder="Email"><br><br>
-    <input type="password" name="senha"id="senha" placeholder="Senha"><br><br>
-    <input type="tel" name="telefone" id="telefone" placeholder="(11) 99999-9999"><br><br>
-    <input type="text" name="cpf" id="cpf" placeholder="999.999.999-99"><br><br>
+    <div class="col-md-12 text-center">
+        <h2>Cadastro de Usuário</h2>
+    </div>
+    <form id="form-cadastro">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="form-label" for="nome">Nome Completo</label>
+                    <input class="form-control" type="text" name="nome" id="nome" placeholder="Nome">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label" for="nome">CPF</label>
+                    <input class="form-control" type="text" name="cpf" id="cpf" placeholder="999.999.999-99">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label" for="email">E-mail</label>
+                    <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label" for="senha">Senha</label>
+                    <input class="form-control" type="password" name="senha" id="senha" placeholder="••••••••••">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label" for="telefone">Telefone</label>
+                    <input class="form-control" type="tel" name="telefone" id="telefone" placeholder="(11) 99999-9999">
+                </div>
+                <div class="col-md-12">
+                    <button class="btn btn-success" type="submit">Cadastrar</button>
+                </div>
+            </div>
+        </div>
 
-    <button type="submit">Cadastrar</button>
-</form>
+    </form>
 
-<div id="resposta"></div>
+    <div id="resposta"></div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-<script src="../assets/js/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <script src="../assets/js/main.js"></script>
 
 </body>
+
 </html>
